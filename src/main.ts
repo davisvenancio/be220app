4c8feeebd4c6604c0269ab21ab86f4958f105f04
davisvenancio <venanciodavid11@gmail.com>
2024-05-22 11:26:31 -0300

    (feat) adicionando configuração firebase

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +19,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"be220app","appId":"1:1007356248901:web:b699023999a796fb13342c","storageBucket":"be220app.appspot.com","apiKey":"AIzaSyC3znPZu7KG-DD5-D37u46OskB3GZECHJQ","authDomain":"be220app.firebaseapp.com","messagingSenderId":"1007356248901"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
   ],
 });
