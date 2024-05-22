55041538c138f8ea7812c2489a55e32ebada43aa
davisvenancio <venanciodavid11@gmail.com>
2024-05-22 10:41:15 -0300

    (fix) corrigindo training programs

--- src/app/components/training-programs/training-programs.component.ts
+++ src/app/components/training-programs/training-programs.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons} from '@ionic/angular/standalone';
+import { Observable } from 'rxjs';
+import { ProgramsType } from 'src/app/models/programs-type.model';
+import { ProgramsTypeService } from 'src/app/services/programs-type.service';
 @Component({
   selector: 'app-training-programs',
   templateUrl: './training-programs.component.html',
   styleUrls: ['./training-programs.component.scss'],
   standalone: true,
-  imports: [ IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons]
+  imports: [ IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons, CommonModule]
 })
 export class TrainingProgramsComponent  implements OnInit {
 
-  constructor() { }
+  programsTypes$: Observable<ProgramsType[]> | undefined;
+  constructor(private programsTypeService: ProgramsTypeService) { }
 
   ngOnInit() {
-    console.log('TrainingProgramsComponent ngOnInit');
+    this.programsTypes$ = this.programsTypeService.getProgramsType();
   }
 
 }
