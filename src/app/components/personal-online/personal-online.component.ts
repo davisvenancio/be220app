1dd5597a5c6ee0a6048160398f1d0b67ce91ba75
davisvenancio <venanciodavid11@gmail.com>
2024-05-22 10:31:06 -0300

    (fix) corrigindo personal online

--- src/app/components/personal-online/personal-online.component.ts
+++ src/app/components/personal-online/personal-online.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { addSharp } from 'ionicons/icons';
+import { addSharp, playCircleSharp } from 'ionicons/icons';
+import { Observable } from 'rxjs';
+import { TrainingType } from 'src/app/models/training-type.model';
+import { TrainingTypeService } from 'src/app/services/training-type.service';
 @Component({
   selector: 'app-personal-online',
   templateUrl: './personal-online.component.html',
   styleUrls: ['./personal-online.component.scss'],
   standalone: true,
-  imports: [ IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons]
+  imports: [ IonGrid,IonCol, IonRow, IonAvatar, IonText, IonButton, IonIcon, IonButtons, CommonModule]
 })
 export class PersonalOnlineComponent  implements OnInit {
   
-
-  constructor() { 
-    addIcons({ addSharp });
+  trainingTypes$: Observable<TrainingType[]> | undefined;
+  
+  constructor(private trainingTypeService: TrainingTypeService) { 
+    addIcons({ addSharp,playCircleSharp });
   }
 
   ngOnInit() {
-    console.log('PersonalOnlineComponent ngOnInit');
-  }
+    this.trainingTypes$ = this.trainingTypeService.getTrainingTypes();
+  } 
 
 }
